eb5ac8a02556b38d092d40fcd2c7bd04f2098267
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-06-16 17:17:52 +0300

    Add child window

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
+function createWindow (layout = 'index.html', opts) {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
-    show: false
+    show: false,
+    ...opts
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  win.loadFile(layout)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // win.webContents.openDevTools()
+
+  return win
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  console.log('[createWindow-call]')
+
+  const mainWindow = createWindow()
 
   mainWindow.on('error', (e) => {
     console.error('[mainWindow-error]:', e)
@@ -28,21 +40,27 @@ function createWindow () {
   mainWindow.once('ready-to-show', () => {
     console.log('[ready-to-show]')
 
+    const childWindow = createWindow(
+      'app-init.html',
+      {
+        width: 400,
+        height: 400,
+        frame: false,
+        resizable: false,
+        parent: mainWindow
+      }
+    )
+    childWindow.once('ready-to-show', () => {
+      childWindow.show()
+    })
+
     setTimeout(() => {
       console.log('[show-call]')
 
+      childWindow.hide()
       mainWindow.show()
     }, 10000)
   })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  console.log('[createWindow-call]')
-
-  createWindow()
   
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
@@ -55,7 +73,7 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
